9d1eeda419bd4eb7f5b1d5d8a4324815f27dcc3c
Hugodmota <hugo_mota100@hotmail.com>
2023-03-07 08:21:00 -0300

    Envio inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+{path:'inicio', component: InicioComponent},
+{path:'login', component: LoginComponent},
+{path:'', redirectTo: '/inicio', pathMatch:'full'}
+
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

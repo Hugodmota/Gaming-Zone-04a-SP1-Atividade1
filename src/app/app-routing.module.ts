e332846edcc45ebb8d655a8aa586c474c65764cc
Hugodmota <hugo_mota100@hotmail.com>
2023-03-08 21:51:41 -0300

    Atividade 3 - API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
+import { GuardGuard } from './guard.guard';
+import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { RestritoComponent } from './restrito/restrito.component';
 
 const routes: Routes = [
 
-{path:'inicio', component: InicioComponent},
-{path:'login', component: LoginComponent},
-{path:'', redirectTo: '/inicio', pathMatch:'full'}
-
-
-];
-
-
+  {path:'inicio', component: InicioComponent},
+  {path:'login', component: LoginComponent},
+  {path: 'restrito',component: RestritoComponent, canActivate: [GuardGuard]},
+  {path:'', redirectTo: '/inicio', pathMatch:'full'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
